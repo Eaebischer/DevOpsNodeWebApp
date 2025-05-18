5ed3bfb8e55e21715828a06b91065bb2815e2dda
EAebischer <etienne.aebischer@gmail.com>
2025-05-18 20:29:32 +0200

    change

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2025 DevOps Course! Es ist Sonntag');
+  res.send('Hello FS2025 DevOps Course! Es ist Sonntag. Cloud Deploy auf Render.com');
 });
 
 app.listen(PORT, HOST);
