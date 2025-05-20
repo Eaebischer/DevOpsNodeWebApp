42bc80ba642b7789600df0408e8c1563c6698f65
Eaebischer <143228238+Eaebischer@users.noreply.github.com>
2025-05-20 13:32:23 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2025 DevOps Course! Es ist Sonntag. Cloud Deploy auf Render.com');
+  res.send('Hello FS2025 DevOps Course! Es ist Sonntag. Cloud Deploy auf Render.com. Hiermit soll die Änderung für den Azure Deploy greifen.');
 });
 
 app.listen(PORT, HOST);
